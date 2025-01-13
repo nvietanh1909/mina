8850bba17fc6630ec4aaf20d21dbac70f9457fec
nvietanh1909 <nvietanh.190904@example.com>
2025-01-13 14:37:17 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/mina/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.mina
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:mina/app/app.dart';
+import 'package:mina/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
