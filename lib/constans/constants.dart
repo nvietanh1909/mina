90fb906362d898c4135dc1d3c210e34da6b63675
nvietanh1909 <nvietanh.190904@example.com>
2025-02-14 09:48:47 +0700

    + method otp

--- lib/constans/constants.dart
+++ lib/constans/constants.dart
@@ -1,4 +1,4 @@
 class AppConfig {
-  static const String baseUrl = 'http://192.168.1.8:5000';
+  static const String baseUrl = 'http://192.168.1.4:5000';
   static const String tokenKey = 'auth_token';
 }
